17475d2ed1685445a66a05cc204477a818c877f1
BensonGitau <bensonkamaugitau@gmail.com>
2024-03-22 09:36:13 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array){
+    return array.slice(0, 2);
+}
+
+
+//should return an array of the last two drivers
+const returnLastTwoDrivers = function(array){
+    return array.slice(-2);
+}
+
+
+    
+//has the `returnFirstTwoDrivers` function to as its first element
+//has the `returnLastTwoDrivers` function to as its last element
+//allows us to invoke either function from the array
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+//returns a function
+//should multiply a given value using the created multiplier
+function createFareMultiplier(x){
+    return function (fare){
+        return fare * x;
+    };
+
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+
+
+function selectDifferentDrivers(drivers, func){
+    return func(drivers);
+}
